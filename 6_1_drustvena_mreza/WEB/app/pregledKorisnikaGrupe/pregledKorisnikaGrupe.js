59da8e90062eab9f6669f60ec7769c98742db0e0
sekicki <sekicki@hotmail.rs>
2025-05-21 21:59:53 +0200

    Poceo raditi na delu B.3

--- 6_1_drustvena_mreza/WEB/app/pregledKorisnikaGrupe/pregledKorisnikaGrupe.js
+++ 6_1_drustvena_mreza/WEB/app/pregledKorisnikaGrupe/pregledKorisnikaGrupe.js
@@ -18,7 +18,7 @@ class Grupa{
 }
 
 const urlParams = new URLSearchParams(window.location.search)
-const grupaId = urlParams.get('grupaId') // Preuzimamo vrednost grupaId parametra upita
+const grupaId = [urlParams.get('grupaId')] // Preuzimamo vrednost grupaId parametra upita
 const grupaIme = urlParams.get('grupaIme')//Preuzimamo vrednost grupaime parametra upita
 
 function loadKorisnikeGrupe(){
@@ -76,7 +76,64 @@ function createDataTable(korisniciGrupe) {
         tbody.appendChild(row)
     }
 }
+
+function loadKorisnikaBezGrupe(){
+  fetch(`http://localhost:14117/api/korisnik`) 
+    .then(response => {
+      if (!response.ok) {
+        throw new Error('Request failed. Status: ' + response.status)
+      }
+      return response.json()
+    })
+    .then(korisniciGrupe2 => createDataTable2(korisniciGrupe2))  
+    .catch(error => {                  
+      console.error('Error:', error.message)
+      alert('An error occurred while loading the data. Please try again.')
+    })
+}
+function createDataTable2(korisniciGrupe) {
+
+    let container = document.querySelector(".sec-content") 
+
+    if (!korisniciGrupe || korisniciGrupe.length === 0) {
+        container.innerHTML = `<p> U grupi ne postoji ni jedan korisnik</p>`
+        return    
+    }
     
+    container.innerHTML = `
+        <th>
+        <table class="user-data">
+            <thead class="user-data-head">
+                <tr>
+                    <th>Id</th> 
+                    <th>Korisničko Ime</th> 
+                    <th>Ime</th> 
+                    <th>Prezime</th> 
+                    <th>Datum rodjenja</th> 
+                </tr>
+            </thead>
+            <tbody id="user-data-body">
+            </tbody>
+        </table>
+    `
+    const tbody = container.querySelector("#user-data-body")
+
+    for (let korisnik of korisniciGrupe) {
+        if(korisnik.grupeKorisnika == ""){
+            const row = document.createElement("tr")
+            row.innerHTML = `
+            <td>${korisnik.id}</td>
+            <td>${korisnik.korisnickoIme}</td>
+            <td>${korisnik.ime}</td>
+            <td>${korisnik.prezime}</td>
+            <td>${formatDate(korisnik.datumRodjenja)}</td>
+            `
+        tbody.appendChild(row)
+        }
+        
+    }
+}
+
 
 function formatDate(isoDateString) {
     const date = new Date(isoDateString)
@@ -84,3 +141,4 @@ function formatDate(isoDateString) {
 }
 
 document.addEventListener('DOMContentLoaded', loadKorisnikeGrupe)
+document.addEventListener('DOMContentLoaded', loadKorisnikaBezGrupe)
